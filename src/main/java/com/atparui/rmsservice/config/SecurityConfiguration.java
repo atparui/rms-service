8b4098669299599195db2070d896f60f91c95501
shivain22 <shivain22@gmail.com>
2026-01-27 12:35:18 +0530

    FIX: EnableWebSecurity to ensure HttpSecurity is configured
    Spring Boot was not creating the HttpSecurity bean (likely due to reactive stack presence). Explicitly enabling web security ensures HttpSecurityConfiguration is imported.

--- src/main/java/com/atparui/rmsservice/config/SecurityConfiguration.java
+++ src/main/java/com/atparui/rmsservice/config/SecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
@@ -30,6 +31,7 @@ import org.springframework.security.web.header.writers.XXssProtectionHeaderWrite
 import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
 
